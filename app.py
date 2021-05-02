c6b86458b330098ca2f774360396b435e4616c75
sanix <sanicksikani@gmail.com>
2021-05-02 19:35:56 +0300

    Using Flask

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from weasyprint import HTML
-import flask
+from flask import Flask, render_template
+import os 
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('invoice.html')
 
 if __name__=='__main__':
-    html=HTML('invoice.html')
-    html.write_pdf('invoice.pdf')
+    port=int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)    
